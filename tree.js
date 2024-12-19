7fb756999a01695dd912d6fa6b042b2067292cbd
Carson Hawk <carsonkhawk@gmail.com>
2024-12-19 15:08:12 -0500

    Initial load

---
+++ tree.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    // Logic can go here.
+    document.getElementById("tree").addEventListener("click", () =>{
+
+    })
+    document.getElementById("orn1").addEventListener("click", () => {})
+    document.getElementById("orn2").addEventListener("click", () => {})
+    document.getElementById("orn3").addEventListener("click", () => {})
+    document.getElementById("orn4").addEventListener("click", () => {})
+    document.getElementById("orn5").addEventListener("click", () => {})
+    document.getElementById("orn6").addEventListener("click", () => {})
+    document.getElementById("orn7").addEventListener("click", () => {})
+    document.getElementById("orn8").addEventListener("click", () => {})
+
+    document.getElementById("gift1").addEventListener("click", () => {})
+    document.getElementById("gift2").addEventListener("click", () => {})
+    document.getElementById("gift3").addEventListener("click", () => {})
+})
+
