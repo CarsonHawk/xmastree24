b38eeb2664c8aa21dca7b0e6593fbd77cff5d5c4
Carson Hawk <carsonkhawk@gmail.com>
2024-12-23 22:43:05 -0500

    Tree burns now.

--- tree.js
+++ tree.js
@@ -1,23 +1,29 @@
 // Some variables.
 let brokenOrns = new Array;
-let giftIndex = ['teddy bear', 'toy train', 'letter blocks', 'PS2', 'baseball', 'lump of coal', 'huge d20', 'brick cellphone', 'camera', 'book'];
+const giftIndex = ['teddy bear', 'toy train', 'letter blocks', 'PS2', 'baseball', 'lump of coal', 'huge d20', 'brick cellphone', 'camera', 'book'];
 let pictureIsSwapped = false;
+let treeIsBurntAlready = false;
 
 document.addEventListener("DOMContentLoaded", () => {
     // Logic can go here.
     document.getElementById("tree").addEventListener("click", () => {
+        console.log(brokenOrns);
+        if (ornamentChecker() && !treeIsBurntAlready) {
+            console.log("here!");
+            burnTree(document.getElementById("tree"));
+        }
 
     })
     document.getElementById("snowman").addEventListener("click", () => {
         snowmanWave();
     })
-    document.getElementById("painting").addEventListener("click", () =>{
+    document.getElementById("painting").addEventListener("click", () => {
         let elem = document.getElementById("painting");
-        if(!pictureIsSwapped){
+        if (!pictureIsSwapped) {
             swapImage(elem, "picture1.png");
             pictureIsSwapped = true;
             console.log(pictureIsSwapped);
-        }else{
+        } else {
             swapImage(elem, "picture0.png");
             pictureIsSwapped = false;
         }
@@ -43,7 +49,7 @@ function dropOrnament(ornToDrop) {
     let animInt = null;
     // let pos = ornToDrop.style.top; // This one drops it from the top of the tree, but nicely.
     // let pos = ornToDrop.offsetTop; // This and the parseFloat one just teleport it.
-    let pos = parseFloat(window.getComputedStyle(ornToDrop).getPropertyValue("top").replace('px', ''));
+    let pos = parseFloat(getComputedStyle(ornToDrop).getPropertyValue("top").replace('px', ''));
     clearInterval(animInt);
     animInt = setInterval(moveIt, 1);
     function moveIt() {
@@ -53,6 +59,9 @@ function dropOrnament(ornToDrop) {
             swapImage(ornToDrop, 'ornbrk.png')
             let ornBreak = new Audio('sfx/338691__natemarler__glass-break-trim.wav');
             ornBreak.play();
+            if (!brokenOrns.includes(ornToDrop.id)) {
+                brokenOrns.push(ornToDrop.id);
+            }
         } else {
             pos++;
             ornToDrop.style.top = pos + "px";
@@ -66,33 +75,56 @@ function swapImage(elementToSwap, newImage) {
     elementToSwap.style.backgroundImage = "url(" + newImage + ")";
 }
 
-function rockToPane(){
+function rockToPane() {
 
 }
-function snowmanWave(){
+function snowmanWave() {
     let mans = document.getElementById("snowman");
-        setTimeout(() => {
-            swapImage(mans,"snowman_wav0.png");
-          }, "500");
-        setTimeout(() => {
-            swapImage(mans,"snowman_wav1.png");
-          }, "1000");
-          setTimeout(() => {
-            swapImage(mans,"snowman_wav0.png");
-          }, "1500");
-        setTimeout(() => {
-            swapImage(mans,"snowman_wav1.png");
-          }, "2000");
-          setTimeout(() => {
-            swapImage(mans,"snowman_wav0.png");
-          }, "2500");
-        setTimeout(() => {
-            swapImage(mans,"snowman_wav1.png");
-          }, "3000");
-          setTimeout(() => {
-            swapImage(mans,"snowman_wav0.png");
-          }, "3500");
-          setTimeout(() => {
-            swapImage(mans,"snowman.png");
-          }, "4000");
+    setTimeout(() => {
+        swapImage(mans, "snowman_wav0.png");
+    }, "500");
+    setTimeout(() => {
+        swapImage(mans, "snowman_wav1.png");
+    }, "1000");
+    setTimeout(() => {
+        swapImage(mans, "snowman_wav0.png");
+    }, "1500");
+    setTimeout(() => {
+        swapImage(mans, "snowman_wav1.png");
+    }, "2000");
+    setTimeout(() => {
+        swapImage(mans, "snowman_wav0.png");
+    }, "2500");
+    setTimeout(() => {
+        swapImage(mans, "snowman_wav1.png");
+    }, "3000");
+    setTimeout(() => {
+        swapImage(mans, "snowman_wav0.png");
+    }, "3500");
+    setTimeout(() => {
+        swapImage(mans, "snowman.png");
+    }, "4000");
+}
+
+function ornamentChecker() {
+    if (brokenOrns.includes("orn1") && brokenOrns.includes("orn2") && brokenOrns.includes("orn3") && brokenOrns.includes("orn4") && brokenOrns.includes("orn5") && brokenOrns.includes("orn6") && brokenOrns.includes("orn7") && brokenOrns.includes("orn8")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function burnTree(theTree) {
+    let burn = new Audio('sfx/483321__craigsmith__r28-02-large-fire-burning.wav')
+    burn.play();
+    // Tiny delay before burning the tree.
+    simpleDelay(250).then(() => swapImage(theTree, "flame.gif"));
+    // Delay for 11760 milliseconds (length of the burning audio) then swap to the dead tree.
+    simpleDelay(11760).then(() => swapImage(theTree, "treeDead.png"));
+    // Make sure to set the treeIsBurntAlready variable to TRUE so it won't burn again on next click.
+    treeIsBurntAlready = true;
+}
+
+function simpleDelay(ms) {
+    return new Promise((resolve => setTimeout(resolve, ms)));
 }
